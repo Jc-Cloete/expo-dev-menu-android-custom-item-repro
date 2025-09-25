0522d6a5a3434b351ade9176580174d8d5ad4c4b
Jc Cloete <jc@example.com>
2025-09-25 15:58:01 +0200

    Register a dev-menu item

--- App.js
+++ App.js
@@ -1,6 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Platform } from "react-native";
+import { registerDevMenuItems } from "expo-dev-menu";
+
+if (__DEV__ && Platform.OS !== "web") {
+  registerDevMenuItems([
+    {
+      name: "Reset App State",
+      shouldCollapse: false,
+      callback: () => console.log("Resetting…"),
+    },
+  ])
+    .then(() => console.log("Dev Menu item registered"))
+    .catch((error) => console.warn("Failed to register Dev Menu item", error));
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
